7e0bc5c9da72fd2adb92bedd03f9dbbef00e3955
Andrew Beresford <beezly@beezly.org.uk>
2025-03-03 22:41:05 +0000

    Update

--- mnet.py
+++ mnet.py
@@ -1,6 +1,7 @@
 import serial
 import struct
-import time
+import sys
+import datetime
 from crc import CrcCalculator, Crc16
 
 
@@ -10,35 +11,86 @@ class Mnet:
     MAX_PACKET_SIZE = 300
     REQ_MULTIPLE_DATA = b'\x0c\x2a'
     REQ_DATA = b'\x0c\x28'
+    REQ_COMMAND = b'\x0c\x32'
+    REQ_SERIAL_NUMBER = b'\x0c\x2e'
+    LOGIN_131_GAIA_WIND = b'\x31\x33\x31\x20\x66\x6b\x59\x75\x29\x29\x31\x32\x32\x32\x31\x51\x51\x61\x61\x00'
+    LOGIN_PACKET_ID = 0x7b
+
+    DATA_ID_WIND_SPEED = b'\x9c\x43'
+    DATA_ID_GEN_REVS = b'\x9c\x47'
+    DATA_ID_ROTOR_REVS = b'\x9c\x46'
+    DATA_ID_L1V = b'\x9c\xa5'
+    DATA_ID_L2V = b'\x9c\xa6'
+    DATA_ID_L3V = b'\x9c\xa7'
+    DATA_ID_L1A = b'\x9c\xa9'
+    DATA_ID_L2A = b'\x9c\xaa'
+    DATA_ID_L3A = b'\x9c\xab'
+
+    DATA_ID_SYSTEM_PRODUCTION = b'\x80\xe9'
+    DATA_ID_G1_PRODUCTION = b'\x80\xea'
+    DATA_ID_CONTROLLER_TIME = b'\xc3\x53'
+
+    DATA_ID_GRID_POWER = b'\x9c\xac'
+    DATA_ID_GRID_CURRENT = b'\x9c\xa8'
+    DATA_ID_GRID_VOLTAGE = b'\x9c\xa4'
+
+    DATA_ID_GRID_VAR = b'\x9c\xad'
+
+    DATA_ID_CURRENT_STATUS_CODE = b'\x00\x0c'
+    DATA_ID_EVENT_STACK_STATUS_CODE = b'\x00\x0b'
+
+    DATA_ID_EMPTY = b'\x00\x00'
+    DATA_ID_START = b'\x00\x01'
+    DATA_ID_STOP = b'\x00\x02'
+    DATA_ID_RESET = b'\x00\x03'
 
     class MnetPacket:
-        def __init__(self, destination, source, packet_type, data_len, data, crc):
+        SOH = b'\x01'
+        EOT = b'\x04'
+
+        def __init__(self, destination, source, packet_type, data_len, data, crc=None):
+            self.crc_calculator = CrcCalculator(Crc16.CCITT)
             self.destination = destination
             self.source = source
             self.packet_type = packet_type
             self.data_len = data_len
             self.data = data
-            self.crc = crc
+
+            self.real_data = self.data.replace(b'\xff', b'\xff\xff')
+            self.packet = self.destination+self.source+self.packet_type + \
+                len(self.real_data).to_bytes(1, byteorder='big')+self.real_data
+            self.calculated_crc = self.crc_calculator.calculate_checksum(
+                self.packet)
+            if crc is None:
+                self.crc = self.calculated_crc
+            else:
+                self.crc = crc
 
         def __str__(self):
-            return 'sot:01 dst:{:02x} src:{:02x} type:{:04x} len:{:02x} data:{:s} crc:{:04x} eot:04'.format(self.destination, self.source, self.packet_type, self.data_len, self.data.hex(), self.crc)
+            return 'sot:01 dst:0x{:02s} src:0x{:02s} type:0x{:04s} len:{:d} data:{:s} crc:{:04x} eot:04'.format(
+                self.destination.hex(),
+                self.source.hex(),
+                self.packet_type.hex(),
+                self.data_len,
+                self.data.hex(),
+                self.crc)
+
+        def __bytes__(self):
+            return self.SOH+self.packet+self.crc.to_bytes(2, byteorder='big')+self.EOT
 
     def __init__(self, device, id=b'\x01'):
         self.device = serial.Serial(port=device, baudrate=38400, timeout=2)
         self.id = id
         self.crc_calculator = CrcCalculator(Crc16.CCITT)
+        self.serial = None
+        self.encoded_serial = None
 
     def create_packet(self, destination, packet_type, data):
-        dataff = data.replace(b'\xff', b'\xff\xff')
-        packet = destination+self.id+packet_type + \
-            len(data).to_bytes(1, byteorder='big')+dataff
-        crc = self.crc_calculator.calculate_checksum(
-            packet).to_bytes(2, byteorder='big')
-        return self.SOH+packet+crc+self.EOT
+        return Mnet.MnetPacket(destination, self.id, packet_type, len(data), data)
 
     def send_packet(self, destination, packet_type, data):
         packet = self.create_packet(destination, packet_type, data)
-        self.device.write(packet)
+        self.device.write(bytes(packet))
         return self.read_packet()
 
     def read_packet(self):
@@ -51,8 +103,177 @@ class Mnet:
             data = b''
         tail = self.device.read(3)
         (crc, eot) = struct.unpack('!HB', tail)
-        packet = Mnet.MnetPacket(destination, source,
-                                 packet_type, data_len, data, crc)
+        packet = Mnet.MnetPacket(destination.to_bytes(1, byteorder='big'), source.to_bytes(1, byteorder='big'),
+                                 packet_type.to_bytes(2, byteorder='big'), data_len, data, crc)
         return packet
 
+    def get_serial_number(self, destination):
+        res = self.send_packet(destination, self.REQ_SERIAL_NUMBER, b'')
+        (serial,) = struct.unpack('!L', res.data)
+        return (serial, res.data)
 
+    def encode_serial(self, serial_bytes):
+        (p0, p1, p2, p3) = struct.unpack('!BBBB', serial_bytes)
+        res = bytearray(5)
+        res[0] = ((p2 & p1) - p2) % 256
+        res[1] = (p1 + p0 + p3) % 256
+        res[2] = (p3 + p0 ^ p1) % 256
+        res[3] = ((p3 & p1) + p2) % 256
+        res[4] = ((p3 | p2) - p3) % 256
+        return res
+
+    def encode(self, data, enc_serial):
+        CONST = 0x34
+        ba = bytearray(data)
+        out = [0]*len(data)
+
+        previous_byte = 0
+        for i in range(len(ba)):
+            byte = ba[i]
+            out[i] = ((enc_serial[i % 5] - previous_byte ^ byte) + CONST) % 256
+            previous_byte = byte
+        return bytes(out)
+
+    def decode(self, data, enc_serial):
+        CONST = 0x34
+        ba = bytearray(data)
+        out = [0]*len(data)
+
+        tmp = 0
+
+        for i in range(len(ba)):
+            tmp = (ba[i] - CONST ^ enc_serial[i % 5] - tmp) % 256
+            out[i] = tmp
+        return bytes(out)
+
+    def decode_data(self, data_in):
+        header = data_in[0:5]
+        (type, conversion_type, conversion_value,
+         length) = struct.unpack('!BBHB', header)
+        #print(type, conversion_type, conversion_value, length)
+        # print(data_in)
+        match type:
+            case 0x0:
+                raw_data = None
+            case 0x1, 0xa:
+                (raw_data,) = struct.unpack_from('!b', data_in, 5)
+            case 0x2:
+                (raw_data,) = struct.unpack_from('!b', data_in, 5)
+            case 0x3:
+                (raw_data,) = struct.unpack_from('!h', data_in, 5)
+            case 0x4:
+                (raw_data,) = struct.unpack_from('!H', data_in, 5)
+            case 0x5:
+                (raw_data,) = struct.unpack_from('!l', data_in, 5)
+            case 0x6:
+                (raw_data,) = struct.unpack_from('!L', data_in, 5)
+            case 0x7:
+                (raw_data,) = struct.unpack_from('!L', data_in, 5)
+            case 0x9:
+                (raw_data,) = struct.unpack_from(
+                    f'!{length}s', data_in, 5)
+                raw_data = raw_data.decode('ascii').rstrip('\x00')
+
+        # print(raw_data)
+        match conversion_type:
+            case 0x0:
+                value = raw_data
+            case 0x1 | 0x5:
+                value = float(raw_data)/pow(10, conversion_value)
+            case 0x2:
+                if conversion_value != 0:
+                    value = float(raw_data)/conversion_value
+                else:
+                    value = float(raw_data)
+            case 0x3:
+                if conversion_value != 0:
+                    value = float(raw_data)*conversion_value
+                else:
+                    value = float(raw_data)
+            case 0x4:
+                value = float(raw_data)*pow(10, conversion_value)
+        return (type, value)
+
+    def decode_multiple_data(self, data_in):
+        # print(data_in)
+        num = data_in[0]  # first byte is number of elements
+        pos = 1
+        res = []
+        for i in range(num):  # iterate through x elements
+            next_header = data_in[pos:pos+9]
+            # print(next_header)
+            (mainid, subid, _, _, _, length) = struct.unpack(
+                "!HHBBHB", next_header)
+            next_data = data_in[pos+4:pos+9+length]
+            d = (mainid, subid, self.decode_data(next_data))
+            res.append(d)
+            pos += 9+length
+        return res
+
+    def create_login_packet_data(self):
+        return struct.pack('=20sBBBBBBBBBBBB',
+                           self.LOGIN_131_GAIA_WIND,
+                           0xff,
+                           0xff,
+                           self.LOGIN_PACKET_ID >> 0x18,
+                           self.LOGIN_PACKET_ID >> 0x10,
+                           self.LOGIN_PACKET_ID >> 8,
+                           self.LOGIN_PACKET_ID,
+                           5,
+                           0,
+                           0,
+                           0,
+                           0,
+                           0
+                           )
+
+    def send_command(self, destination, command_id):
+        if self.serial is None:
+            (self.serial, serial_bytes) = self.get_serial_number(destination)
+            self.encoded_serial = self.encode_serial(serial_bytes)
+        response = self.send_packet(
+            destination, self.REQ_COMMAND, command_id)
+        return response
+
+    def request_data(self, destination, data_id, sub_id=0):
+        if self.serial is None:
+            (self.serial, serial_bytes) = self.get_serial_number(destination)
+            self.encoded_serial = self.encode_serial(serial_bytes)
+        response = self.send_packet(
+            destination, self.REQ_DATA, data_id+sub_id.to_bytes(2, byteorder='big'))
+        # print(response)
+        # print(self.encoded_serial)
+        data = self.decode(response.data, self.encoded_serial)
+        (datatype, value) = self.decode_data(data)
+        return value
+
+    def request_multiple_data(self, destination, datasubids, include_ids=False):
+        if self.serial is None:
+            (self.serial, serial_bytes) = self.get_serial_number(destination)
+            self.encoded_serial = self.encode_serial(serial_bytes)
+        id_bytes = bytearray(len(datasubids).to_bytes(1, byteorder='big'))
+        for (m, s) in datasubids:
+            a = m+s.to_bytes(2, byteorder='big')
+            id_bytes += a
+        response = self.send_packet(
+            destination, self.REQ_MULTIPLE_DATA, id_bytes)
+        #print('id:', id_bytes)
+        data = self.decode(response.data, self.encoded_serial)
+        #print('data:', data)
+        decoded_data = self.decode_multiple_data(data)
+        #print('d:', decoded_data)
+        ret_data = []
+        for (main_id, sub_id, (datatype, value)) in decoded_data:
+            if include_ids:
+                ret_data += [(main_id.to_bytes(2, byteorder='big'),
+                              sub_id, value)]
+            else:
+                ret_data += [value]
+        return ret_data
+
+    def timestamp_to_datetime(self, seconds):
+        EPOCH = datetime.datetime(1980, 1, 1)
+        return EPOCH+datetime.timedelta(seconds=seconds)
+
+    def event_log(self, start=0, end=65):
+        DATA_ID_EVENT_STACK_STATUS_CODE
--- test.py
+++ test.py
@@ -1,64 +1,164 @@
 import mnet
+import time
+import bitstring
+import paho.mqtt.client as mqtt
+import json
+
+TOPIC_PREFIX = 'turbine/'
+
+next_command = None
+
+def convert_to_bits(data):
+    return bitstring.Bits(data).bin
+
+
+def bits_string(data):
+    return ' '.join([data[i:i+8] for i in range(0, len(data), 8)])
+
+def handle_command_message(client, userdata, message):
+    print('handling command')
+    global next_command
+    (mnet_client, DEST)=userdata
+    command_payload=message.payload.decode('utf-8')
+    print (f"message recevied: {message.topic}: {command_payload}")
+
+    match command_payload:
+        case "start":
+            print ("setting next_command to start")
+            next_command = mnet.Mnet.DATA_ID_START
+        case "stop":
+            print ("setting next_command to stop")
+            next_command = mnet.Mnet.DATA_ID_STOP
+        case "reset":
+            print ("setting next_command to reset")
+            next_command = mnet.Mnet.DATA_ID_RESET
+        case _:
+            print (f"Unknown Command {command_payload}")
+
+    return
 
 DEST = b'\x02'
 
-test = Mnet('/dev/ttyUSB0')
-# test.send_packet(b'\x02', b'\xea\xe5', b'\x02')
-# z = test.send_packet(DEST, b'\x00\x01', b'')
-# if hex(z.packet_type) != '0x1390':
-#    print(hex(z.packet_type))
+test = mnet.Mnet('/dev/ttyUSB0')
+lastbits = ''
+counter = 0
+mqtt = mqtt.Client(client_id='turbine_mqtt',userdata=(test, DEST))
+mqtt.connect('mqtt.lan')
+mqtt.loop_start()
 
-# for x in range(65536):
-#    bx = x.to_bytes(2, byteorder="big", signed=False)
-#    z = test.send_packet(DEST, bx, b'')
-#        print('sent: {:s} returned: {:s}, {:s}'.format(
-#    if hex(z.packet_type) != '0x1390':
-#            bx.hex(), hex(z.packet_type), str(z)))
+(serial, serial_bytes) = test.get_serial_number(DEST)
+encoded_serial = test.encode_serial(serial_bytes)
+print(f'got serial: {serial}')
 
-#z = test.send_packet(DEST, b'\xea\xe5', b'')
-# print(z)
-# print(test.send_packet(DEST, b'\x0c\x2e',b'')) # SOME SORT OF LOGIN?
-#print(test.send_packet(DEST, Mnet.REQ_DATA,b'\x01\x9c\x47\x00\x00'))
-# LOGIN PACKET?
-print('login')
-print(test.send_packet(DEST, b'\x13\xa1', b'\x09\x7b\x13\xc1\xbe\x49\x97\xf1\x74\xfe\xbe\x7a\x11\xd2\x1f\x16\x39\xe5\x50\xdf\x09\x7a\x10\xdf\x40\x18\xd9\x55\x12\x40\x18\xd9\x55\x12\x40\x18\xd9\x55\x12\x40\x4f\x8d\x56\x12\x40\xd3\x63\x50\x12\x40\x18\xd9'))
-# MODDED LOGIN
-#print(test.send_packet(DEST, b'\x13\xa1',b'\x09\x7b\x13\xc1\xbe\x49\x97\xf1\x74\xfe\xbe\x7a\x11\xd2\x1f\x16\x39\xe5\x50\xdf\x09\x7a\x10\xdf\x40\x00\x00\x00\x00\x40\x00\x00\x00\x00\x40\x00\x00\x00\x00\x00\x00\x00\x00\x00\x40\x00\x00\x00\x00\x40\x00\x00'))
-# VERY MODDED LOGIN
-#print(test.send_packet(DEST, b'\x13\xa1', b'\x09\x7b\x13\xc1\xbe\x49\x97\xf1\x74\xfe\xbe\x7a\x11\xd2\x1f\x16\x39\xe5\x50\xdf\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'))
-print(test.send_packet(DEST, b'\x0c\x28', b'\x00\x04\x00\x00'))
-#print(test.send_packet(DEST, b'\x0c\x28',b'\x00\x04\x00\x00'))
-#print(test.send_packet(DEST, b'\x0c\x2e',b''))
-#print(test.send_packet(DEST, b'\xea\xe5', b'\x04'))
+command_topic = TOPIC_PREFIX+str(serial)+'/command'
+print(command_topic)
+mqtt.message_callback_add(command_topic,handle_command_message)
+mqtt.subscribe(command_topic)
 
 while True:
-    #print('0c28 single 04')
-    #print(test.send_packet(DEST, b'\x0c\x28',b'\x00\x04\x00\x00'))
-    #print('0c28 single 01')
-    #print(test.send_packet(DEST, b'\x0c\x28',b'\x00\x01\x00\x00'))
+    try:
+        data = test.encode(test.create_login_packet_data(), encoded_serial)
+        login = test.send_packet(DEST, b'\x13\xa1', data)
+        # print("login packet:", login)
+        # print("result1: ", test.send_packet(DEST, b'\x13\xa1', data))
+        # controller_time = test.request_data(
+        #    DEST, mnet.Mnet.DATA_ID_CONTROLLER_TIME)
+        # print('controller_time:', controller_time)
 
-    #print('brake ops')
-    # print(test.send_packet(DEST, Mnet.REQ_MULTIPLE_DATA,b'\x01\x7d\x0a\x00\x00')) # brake operation count
-    # print(test.send_packet(DEST, Mnet.REQ_MULTIPLE_DATA,b'\x01\x80\xea\x00\x65')) # g1 production - produced
-    #print('g1 prod multiple')
-    #print('g1 prod single')
-    # print(test.send_packet(DEST, Mnet.REQ_DATA,b'\x80\xea\x00\x65')) # g1 prod?
-    #print('stopped time')
-    # print(test.send_packet(DEST, Mnet.REQ_DATA, b'\x7d\x05\x00\x00')) # stopped time
-    # print(test.send_packet(DEST, Mnet.REQ_DATA, b'\x7d\x06\x00\x00')) # g1 operation time
-    #print('yaw 2 temp')
-    #print(test.send_packet(DEST, Mnet.REQ_DATA, b'\xf2\x30\x00\x01'))
+        # print(next_command)
+        if next_command != None:
+            print(f"sending command: {next_command}")
+            try: 
+                r=test.send_command(DEST, next_command)
+                print(r)
+            finally:
+                next_command = None
 
-    #print('controller time')
-    # print(test.send_packet(DEST, Mnet.REQ_DATA,b'\x4e\x26\x00\x00')) # controller time
-    print('rotor revolutions')
-    print(test.send_packet(DEST, Mnet.REQ_MULTIPLE_DATA,
-          b'\x02\x9c\x46\x00\x00\x9c\x47\x00\x00'))
-    print(test.send_packet(DEST, Mnet.REQ_DATA,              b'\x9c\x46\x00\x00'))
-    print(test.send_packet(DEST, Mnet.REQ_DATA,              b'\x9c\x47\x00\x00'))
-    time.sleep(1)
-#print(test.send_packet(DEST, Mnet.REQ_DATA,b'\x01\xc3\x53\x00\x00'))
-#print(test.send_packet(DEST, Mnet.REQ_DATA,b'\x01\xc3\x53\x00\x00'))
-#print(test.send_packet(DEST, Mnet.REQ_DATA,b'\x01\xc3\x53\x00\x00'))
-#print(test.send_packet(DEST, Mnet.REQ_DATA,b'\x01\xc3\x53\x00\x00'))
-#print(test.send_packet(DEST, Mnet.REQ_DATA,b'\x01\xc3\x53\x00\x00'))
+        gen_revs = test.request_data(DEST, mnet.Mnet.DATA_ID_GEN_REVS)
+        rotor_revs = test.request_data(DEST, mnet.Mnet.DATA_ID_ROTOR_REVS)
+        wind = test.request_data(DEST, mnet.Mnet.DATA_ID_WIND_SPEED)
+
+        l1v = test.request_data(DEST, mnet.Mnet.DATA_ID_L1V)
+        l2v = test.request_data(DEST, mnet.Mnet.DATA_ID_L2V)
+        l3v = test.request_data(DEST, mnet.Mnet.DATA_ID_L3V)
+        # l1a = test.request_data(DEST, mnet.Mnet.DATA_ID_L1A)
+        # l2a = test.request_data(DEST, mnet.Mnet.DATA_ID_L2A)
+        # l3a = test.request_data(DEST, mnet.Mnet.DATA_ID_L3A)
+        # l1p = l1v*l1a
+        # l2p = l2v*l2a
+        # l3p = l3v*l3a
+        # total_power = l1p+l2p+l3p
+
+        # print(
+        #    f'wind: {wind:.0f}m/s',
+        #    f'rotor: {rotor_revs:.0f}rpm',
+        # f'ph1: ( {l1v:.0f}v {l1a:.2f}a {l1p:.0f}W )',
+        # f'ph2: ( {l2v:.0f}v {l2a:.2f}a {l2p:.0f}W )',
+        ##    f'gen: {gen_revs:.0f}rpm'
+        #    # f'ph3: ( {l3v:.0f}v {l3a:.2f}a {l3p:.0f}W )',
+        # f'total_power: {total_power:.0f}W'
+        # )
+
+        # test_1 = test.request_data(DEST, mnet.Mnet.DATA_ID_TEST_1)
+        # test_2 = test.request_data(DEST, mnet.Mnet.DATA_ID_TEST_2)
+        # print(test_1, test_2, test_3)
+        # test_3 = test.request_data(DEST, mnet.Mnet.DATA_ID_TEST_3)
+        # print(test.request_data(DEST, b'\x9c\xac'))
+        # print(test.request_data(DEST, b'\x9c\xa8'))
+        power = test.request_data(DEST, mnet.Mnet.DATA_ID_GRID_POWER)
+        #print('power: ', power)
+        # for i in [1, 2, 3, 11, 12, 13, 14, 15, 16, 21, 22, 23, 24, 26, 27, 31, 32, 33, 34, 35, 36, 37, 41, 42, 43, 44, 45, 51, 52, 53, 54, 55, 56, 57, 61, 62, 63, 64]:
+        #    b = i*100
+        #    for y in range(31):
+        # for i in [1123, 1124, 1224, 4101, 4102, 10001, 101, 102, 201, 202]:
+        #    print('test_prod_g1: ', i, test.request_data(DEST,
+        #                                                 mnet.Mnet.DATA_ID_G1_PRODUCTION, i))
+        #    print('test_prod_sys: ', i, test.request_data(DEST,
+        #                                                  mnet.Mnet.DATA_ID_SYSTEM_PRODUCTION, i))
+        # print('multi: ', test.request_multiple_data(DEST, [
+        #      (mnet.Mnet.DATA_ID_GRID_VOLTAGE, 101), (mnet.Mnet.DATA_ID_GRID_VOLTAGE, 102)]))
+        #print('var: ', test.request_data(DEST, mnet.Mnet.DATA_ID_GRID_VAR, 101))
+        #print('gv: ', test.request_data(DEST, mnet.Mnet.DATA_ID_GRID_VOLTAGE, 101))
+        #print('system_production', test.request_data(
+        #    DEST, mnet.Mnet.DATA_ID_SYSTEM_PRODUCTION, 101))
+        # print('g1_production', test.request_data(
+        #    DEST, mnet.Mnet.DATA_ID_G1_PRODUCTION, 100))
+        # print('current_status_code', test.request_data(
+        #    DEST, mnet.Mnet.DATA_ID_CURRENT_STATUS_CODE, 1))
+        #print(test.request_data(DEST, mnet.Mnet.DATA_ID_CURRENT_STATUS_CODE, 101))
+        # for i in range(10):
+        #    data_list = [
+        #        (mnet.Mnet.DATA_ID_EVENT_STACK_STATUS_CODE, i*100),
+        #        (mnet.Mnet.DATA_ID_EVENT_STACK_STATUS_CODE, i*100+1),
+        #        (mnet.Mnet.DATA_ID_EVENT_STACK_STATUS_CODE, i*100+2)]
+        #    status_line = test.request_multiple_data(DEST, data_list)
+        #    [status_code, ts, message] = status_line
+        #    print('status: ',
+        #          test.timestamp_to_datetime(ts),
+        #          message,
+        #          status_code
+        #          )
+        latest_status_message = test.request_data(
+            DEST, mnet.Mnet.DATA_ID_EVENT_STACK_STATUS_CODE, 2).strip()
+        mqtt_data = {
+            'wind_speed_mps': wind,
+            'rotor_rpm': rotor_revs,
+            'generator_rpm': gen_revs,
+            'power_W': power,
+            'status_message': latest_status_message,
+            'l1v': l1v,
+            'l2v': l2v,
+            'l3v': l3v
+        }
+        print(mqtt_data)
+        pub_data = mqtt.publish(TOPIC_PREFIX+str(serial), json.dumps(mqtt_data))
+        pub_data.wait_for_publish()
+        # print(pub_data.is_published)
+
+        time.sleep(1)
+    except Exception as ex:
+        print("EXCEPTION")
+        print(type(ex))
+        print(ex.args)
+        print(ex)
+        time.sleep(10)
